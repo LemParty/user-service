d091f7cf14839e985a651286e16353d04ace23bf
kqlam21 <kqlam21@gmail.com>
2020-08-26 14:45:54 -0700

    Update User.java
    to include more columns

--- src/main/java/com/lemparty/entity/User.java
+++ src/main/java/com/lemparty/entity/User.java
@@ -1,13 +1,13 @@
 package com.lemparty.entity;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Column;
-import javax.persistence.GenerationType;
 import java.io.Serializable;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "users")
@@ -20,6 +20,22 @@ public class User implements Serializable {
 
     @Column(name = "password")
     private String password;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "dob")
+    private Date dob;
+
+    @Column(name = "creation_date")
+    private Timestamp creation_date;
+
+    @Column(name= "modified_date")
+    private Timestamp modified_date;
+
 
     public String getUserID() {
         return userID;
@@ -43,5 +59,45 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public Timestamp getCreation_date() {
+        return creation_date;
+    }
+
+    public void setCreation_date(Timestamp creation_date) {
+        this.creation_date = creation_date;
+    }
+
+    public Timestamp getModified_date() {
+        return modified_date;
+    }
+
+    public void setModified_date(Timestamp modified_date) {
+        this.modified_date = modified_date;
     }
 }
